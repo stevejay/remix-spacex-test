036dba2e89dcc79720972c429c67a48e53332eb5
Steve Johns <stevej@ocula.tech>
2022-08-30 16:49:31 +0100

    use within more often in cypress tests

--- cypress/e2e/launchesSearch.cy.ts
+++ cypress/e2e/launchesSearch.cy.ts
@@ -13,13 +13,14 @@ describe("launches search tests", () => {
       "have.text",
       "Enter the name of a launch to search for"
     );
-    cy.findByRole("form").should("exist");
-    cy.findByRole("searchbox")
-      .should("have.value", "")
-      .and("have.attr", "placeholder", "Enter a launch name")
-      .and("be.focused");
-    cy.findByRole("button", { name: "Search" }).should("exist");
-    cy.findByRole("link", { name: "Clear" }).should("exist");
+    cy.findByRole("form", { name: "Search launches" }).within(() => {
+      cy.findByRole("searchbox", { name: "Search launches" })
+        .should("have.value", "")
+        .and("have.attr", "placeholder", "Enter a launch name")
+        .and("be.focused");
+      cy.findByRole("button", { name: "Search" }).should("exist");
+      cy.findByRole("link", { name: "Clear" }).should("exist");
+    });
     cy.get("table").should("not.exist");
   });
 
@@ -33,14 +34,14 @@ describe("launches search tests", () => {
 
   it("displays search results if the user searches for a valid search term", () => {
     cy.findByRole("searchbox").type("star{enter}");
-    cy.get("table").should("exist");
+    cy.findByRole("table").should("exist");
     cy.findByRole("searchbox").should("have.value", "star");
     cy.findByRole("alert").contains(/\d+ results? returned/);
   });
 
   it("allows the user to clear the search term and search results", () => {
     cy.findByRole("searchbox").type("star{enter}");
-    cy.get("table").should("exist");
+    cy.findByRole("table").should("exist");
     cy.findByRole("link", { name: "Clear" }).click();
     cy.findByRole("searchbox").should("have.value", "");
     cy.get("table").should("not.exist");
@@ -57,72 +58,97 @@ describe("launches search tests", () => {
 
   it("displays the search results in a table", () => {
     cy.findByRole("searchbox").type("CRS-22 & IROSA{enter}"); // Should return a single launch.
-    cy.findByRole("table").should("exist");
-    cy.findByRole("table")
-      .findByRole("button", { name: "Name" })
-      .should("exist");
-    cy.findByRole("table")
-      .findByRole("button", { name: "Date" })
-      .should("exist");
-    cy.findByRole("table")
-      .findByTestId("5fe3af84b3467846b3242161") // ID for 'CRS-22 & IROSA' launch
-      .within(($row) => {
+    cy.findByRole("table").within(() => {
+      cy.findByRole("button", { name: "Name" }).should("exist");
+      cy.findByRole("button", { name: "Date" }).should("exist");
+      cy.findByTestId("5fe3af84b3467846b3242161").within(() => {
         cy.findByRole("rowheader", { name: "CRS-22 & IROSA" });
         cy.findByRole("cell", { name: "Thu, 03 Jun 2021 17:29:00 GMT" });
         cy.findByRole("link", { name: "View details" });
       });
+    });
   });
 
   it("allows the user to view the details of a launch", () => {
     cy.findByRole("searchbox").type("CRS-22 & IROSA{enter}"); // Should return a single launch.
-    cy.findByRole("link", { name: "View details" }).click();
+    cy.findByRole("table").within(() => {
+      cy.findByRole("link", { name: "View details" }).click();
+    });
     cy.url().should("include", "/spacex/launches/5fe3af84b3467846b3242161");
   });
 
   it("allows the user to change the sorting field and sorting direction of the results table", () => {
     cy.findByRole("searchbox").type("star{enter}");
-    cy.findByRole("table").should("exist");
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Name" })
-      .should("have.attr", "aria-sort", "none");
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Date" })
-      .should("have.attr", "aria-sort", "descending");
-    cy.url().should("include", "sortAscending=false");
-    cy.url().should("include", "sortField=date_utc");
-    cy.url().should("include", "name=star");
 
-    cy.findByRole("table").findByRole("button", { name: "Date" }).click();
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Name" })
-      .should("have.attr", "aria-sort", "none");
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Date" })
-      .should("have.attr", "aria-sort", "ascending");
-    cy.url().should("include", "sortAscending=true");
-    cy.url().should("include", "sortField=date_utc");
-    cy.url().should("include", "name=star");
+    cy.findByRole("table").within(() => {
+      cy.findByRole("columnheader", { name: "Name" }).should(
+        "have.attr",
+        "aria-sort",
+        "none"
+      );
+      cy.findByRole("columnheader", { name: "Date" }).should(
+        "have.attr",
+        "aria-sort",
+        "descending"
+      );
+    });
+    cy.url()
+      .should("include", "sortAscending=false")
+      .should("include", "sortField=date_utc")
+      .should("include", "name=star");
 
-    cy.findByRole("table").findByRole("button", { name: "Name" }).click();
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Name" })
-      .should("have.attr", "aria-sort", "ascending");
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Date" })
-      .should("have.attr", "aria-sort", "none");
-    cy.url().should("include", "sortAscending=true");
-    cy.url().should("include", "sortField=name");
-    cy.url().should("include", "name=star");
+    cy.findByRole("table").within(() => {
+      cy.findByRole("button", { name: "Date" }).click();
+      cy.findByRole("columnheader", { name: "Name" }).should(
+        "have.attr",
+        "aria-sort",
+        "none"
+      );
+      cy.findByRole("columnheader", { name: "Date" }).should(
+        "have.attr",
+        "aria-sort",
+        "ascending"
+      );
+    });
+    cy.url()
+      .should("include", "sortAscending=true")
+      .should("include", "sortField=date_utc")
+      .should("include", "name=star");
 
-    cy.findByRole("table").findByRole("button", { name: "Name" }).click();
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Name" })
-      .should("have.attr", "aria-sort", "descending");
-    cy.findByRole("table")
-      .findByRole("columnheader", { name: "Date" })
-      .should("have.attr", "aria-sort", "none");
-    cy.url().should("include", "sortAscending=false");
-    cy.url().should("include", "sortField=name");
-    cy.url().should("include", "name=star");
+    cy.findByRole("table").within(() => {
+      cy.findByRole("button", { name: "Name" }).click();
+      cy.findByRole("columnheader", { name: "Name" }).should(
+        "have.attr",
+        "aria-sort",
+        "ascending"
+      );
+      cy.findByRole("columnheader", { name: "Date" }).should(
+        "have.attr",
+        "aria-sort",
+        "none"
+      );
+    });
+    cy.url()
+      .should("include", "sortAscending=true")
+      .should("include", "sortField=name")
+      .should("include", "name=star");
+
+    cy.findByRole("table").within(() => {
+      cy.findByRole("button", { name: "Name" }).click();
+      cy.findByRole("columnheader", { name: "Name" }).should(
+        "have.attr",
+        "aria-sort",
+        "descending"
+      );
+      cy.findByRole("columnheader", { name: "Date" }).should(
+        "have.attr",
+        "aria-sort",
+        "none"
+      );
+    });
+    cy.url()
+      .should("include", "sortAscending=false")
+      .should("include", "sortField=name")
+      .should("include", "name=star");
   });
 });
